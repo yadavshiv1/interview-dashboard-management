104a3dcaebb3bcec3053919553e96540e87a95dd
yadavshiv1 <shivnarayanyadav210@gmail.com>
2025-10-03 11:41:08 +0530

    added favicon ico

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="images/favicon.ico" />
+      </head>
       <body className={inter.className}>
         <SnackbarProvider>
           <AuthProvider>
